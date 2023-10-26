4fac36ab118c2bf7e08100d621b03fb3544001fd
Student <Student@PC-SE5113-17>
2023-10-26 11:19:05 +0200

    Add project files.

---
+++ stringy/stringy.cpp
@@ -0,0 +1,38 @@
+﻿// stringy.cpp : Defines the entry point for the application.
+//
+
+#include "stringy.h"
+
+int count_letters(char* text) 
+{
+	int count = 0;
+	for (size_t i = 0; i < strlen(text); i++)
+	{
+		if (isalpha(text[i]))
+		{
+			count++;
+		}
+	}
+	return count;
+}
+
+int count_numbers(char* text)
+{
+	int count = 0;
+	for (size_t i = 0; i < strlen(text); i++)
+	{
+		if (isdigit(text[i]))
+		{
+			count++;
+		}
+	}
+	return count;
+}
+
+int main()
+{
+	char text[] = "Toto jsou 2 vzorove vety, ktere budou zpracovavany Vasemi funkcemi.Auto Hyundai ix35 ma pres 120 konskych sil.";
+	printf("Pocet pismen:%d\n",count_letters(text));
+	printf("Pocet cisel:%d\n", count_numbers(text));
+	return 0;
+}
---
+++ stringy/stringy.h
@@ -0,0 +1,11 @@
+﻿// stringy.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+// TODO: Reference additional headers your program requires here.
